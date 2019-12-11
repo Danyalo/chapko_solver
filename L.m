fa779252da4619ab4fc1225bbae0e13cf11bcbd9
Olenka312 <31958821+Olenka312@users.noreply.github.com>
2019-12-11 06:00:23 +0200

    Update L.m

--- L.m
+++ L.m
@@ -1,30 +1,29 @@
-function [L]=L(X, H, p)
+function [L]=L(X, H, p, r)
 % Computes Lik
 %Inputs:
-% X – points xi, where i is from 1 to n
-% p - point in which we find Lik
+% X â€“ points xi, where i is from 1 to n
+% p - points in which we find Lik
 % r - numer of points on each distance
 % H - step for 1 - n points
 %
 %Outputs:
-% Lik – counted array ????
+% Lik â€“ counted array ????
 % Number of points in X
-n=size(X,1);
+%n=size(X,1);
+n = length(X);
 
-global ModelInfo
-r = ModelInfo.r;
-
-% Compute chyselnyk
-L = [];
+%global ModelInfo
+%r = ModelInfo.r;
+L = [,];
+% Compute Lik
 for i=2:n
-    %hr=(X(i)-X(i-1))/r;
     for k=0:r
-        if (p >= X(i-1) && p <= X(i))
-            point=(p-X(i-1))/H;
-            newXr = linespace(X(i-1), X(i), r);
-            L(i) = Alpha(newXr, point);
+        if (p(i) >= X(i-1) && p(i) <= X(i))
+            point=(p(i)-X(i-1))/H;
+            newXr = linspace(X(i-1), X(i), r);
+            L(i+1,k+1) = Alpha(newXr, point);
         else
-            L(i) = 0;
+            L(i+1,k+1) = 0;
         end
         
     end
