d24d728e3fa865f6b92d772c3f0f0268fd142129
Danylo Labenskyi <danylo.labenskyi@authoritypartners.com>
2019-12-12 02:46:27 +0200

    Fix K function, add array.m

--- K.m
+++ K.m
@@ -5,8 +5,8 @@ if (t ~= theta)
     x_theta = Boundary(theta);
     x_der_theta = BoundaryDer(theta);
     
-    numerator = 2 * ((x_t(0) - x_theta(0)) * x_der_theta(1) ...
-                   - (x_t(1) - x_theta(1)) * x_der_theta(0) );
+    numerator = 2 * ((x_t(1) - x_theta(1)) * x_der_theta(2) ...
+                   - (x_t(2) - x_theta(2)) * x_der_theta(1) );
     denominator = (norm(x_t - x_theta)).^2;
     
     result = numerator / denominator;
@@ -14,8 +14,8 @@ else
     x_t_der = BoundaryDer(t);
     x_t_der2 = BoundaryDer2(t);
     
-    numerator = - x_t_der(0) * x_t_der2(1) ...
-                + x_t_der(1) * x_t_der2(0);
+    numerator = - x_t_der(1) * x_t_der2(2) ...
+                + x_t_der(2) * x_t_der2(1);
     denominator = (norm(x_t_der)).^2;
     
     result = numerator / denominator;
---
+++ array.m
@@ -0,0 +1,5 @@
+function t = array(a,b,n)
+for j=1:n
+    t(j) = (j*b)/n;
+end
+end
