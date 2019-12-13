ed3665850bb601b3e619d12ab1f9f8726086558e
Danylo Labenskyi <danylo.labenskyi@authoritypartners.com>
2019-12-13 22:39:41 +0200

    Fix array function and compute Gauss-Lejandre nodes

--- array.m
+++ array.m
@@ -1,5 +1,5 @@
 function t = array(a,b,n)
 for j=1:n
-    t(j) = (j*b)/n;
+    t(j) = ( (j-1) * b )/ (n - 1);
 end
 end
--- main.m
+++ main.m
@@ -14,10 +14,11 @@ b = 3;
 x1 = @(t)a*cos(t);
 x2 = @(t)b*sin(t);
 
-n =64;
+n = 64;
 x0 = 0;
 xn = 2*pi;
 h = xn/n;
+r = 5;
 
 x1_der =@(t) -a*sin(t);
 x1_der2 =@(t) -a*cos(t);
@@ -27,7 +28,16 @@ x2_der2 =@(t) -b*sin(t);
 t = array(t0,tn,n);
 vector = transpose(arrayfun(f,t));
 matrix = [];
-nodes = lgwt(n, a, b);
+
+for i=1:n-1
+    sigma_i_k = lgwt(r, t(i), t(i+1));
+    % do something with nodes here - or store if there's the need to have
+    % all n * r nodes at once
+end
+
+nodes = lgwt(5, a, b); % example - not useful
+
+
 doubleSum(n, r, t, nodes);
 for i=1:n
     for j=1:n
